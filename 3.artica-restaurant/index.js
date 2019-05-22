3d887fa815c03caff33ff04d1ea2aa123b03559a
trannguyendonggon <trannguyendonggon@gmail.com>
2019-05-22 07:10:19 +0700

    html-css-javascript for banner and menulist

--- 3.artica-restaurant/index.js
+++ 3.artica-restaurant/index.js
@@ -1,4 +1,45 @@
 ﻿document.addEventListener("DOMContentLoaded",function () {
 	new WOW().init();
+
+	var menuItem = document.querySelectorAll('.menu__categorys ul li');
+	var mynav = document.querySelector('.mynav');
+	var added = false;
+	window.addEventListener('scroll',function(){
+
+		if(window.pageYOffset > 100){
+			if(added == false){
+				mynav.classList.add('fixedNav');
+				added = true; // để hàm chỉ xử lý 1 lần khi cuộn cuột
+			}
+		}
+		else{
+			if(added == true){
+				mynav.classList.remove('fixedNav');
+				added = false;
+			}
+		}
+	})
+
+	var iso = new Isotope( '.menu__list', {
+		itemSelector: '.food',
+		layoutMode: 'masonry',
+		masonry: {
+			columnWidth: 540,
+			gutter: 30
+		  }
+	  });
+	// js for menu
+	for(var i = 0; i < menuItem.length; i++){
+		menuItem[i].addEventListener('click',function(){
+			
+			for (let i = 0; i < menuItem.length; i++) {
+				menuItem[i].classList.remove('active');
+			}
+			this.classList.add('active');
+
+			var attr = this.getAttribute('data-filter');
+			iso.arrange({ filter: attr });
+		})
+	}
 },false)
  
