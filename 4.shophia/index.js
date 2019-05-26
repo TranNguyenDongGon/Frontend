da0882c3bd5583872c84a1f74abbb4ced467a897
trannguyendonggon <trannguyendonggon@gmail.com>
2019-05-26 17:48:36 +0700

    finish shopia

--- 4.shophia/index.js
+++ 4.shophia/index.js
@@ -1,4 +1,31 @@
 ﻿document.addEventListener('DOMContentLoaded',function(){
+    var backtotop = document.querySelector('.backtotop');
+    var trangthai = 'chuatoi';
+    window.addEventListener('scroll',function(){
+        var index = window.pageYOffset;
+        
+        if(index > 1000){
+            if(trangthai == 'chuatoi'){
+                backtotop.classList.add('hienra');
+                trangthai = 'toiroi';
+            }
+        }
+        else{
+            if(trangthai == 'toiroi'){
+                backtotop.classList.remove('hienra');
+                trangthai = 'chuatoi';
 
+            }
+        }
+
+    });
+
+    backtotop.addEventListener('click',function(){
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+          });
+    })
 },false);
  
