1c1429c57076672764aad4c9309d6e9bcfdcff1c
trannguyendonggon <trannguyendonggon@gmail.com>
2019-06-01 20:40:36 +0700

    finish netflix

--- 5.Netflix/index.js
+++ 5.Netflix/index.js
@@ -1,4 +1,25 @@
-﻿ $(function(){
- 
-})  
- 
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const tabItems = document.querySelectorAll('.tab-item');
+    const tabContentItems = document.querySelectorAll('.tab-content-item');
+    //select tab content item
+
+    function selectItem(e) {
+        removeBorder();
+        removeShow();
+        // add border to current tab
+        this.classList.add('tab-border');
+        const tabContentItem = document.querySelector(`#${this.id}-content`);
+
+        tabContentItem.classList.add('show');
+    }
+
+    function removeShow(e) {
+        tabContentItems.forEach(item => item.classList.remove('show'));
+    }
+
+    function removeBorder(e) {
+        tabItems.forEach(item => item.classList.remove('tab-border'));
+    }
+    // listen for tab click
+    tabItems.forEach(item => item.addEventListener('click', selectItem));
+}, false);
